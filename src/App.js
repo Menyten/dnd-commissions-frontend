b8fc4dc5f634f1ac1f69332cd07a51fcf3247b01
joel.pedersen97@gmail.com <joel.pedersen97@gmail.com>
2020-06-03 12:36:32 +0200

    checking if authenticated on page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Start from './views/start/Start';
@@ -9,7 +9,45 @@ import Profile from './views/profile/Profile';
 import Authenticated from './components/app/authenticated/Authenticated';
 import Toast from './components/common/toast/Toast';
 
+import { GlobalContext } from './context/GlobalState';
+import { setUser, setToken } from './context/actions/authActions';
+import { fetchy } from './utils/fetchy';
+
 function App() {
+  const {
+    dispatch,
+    state: { user, token }
+  } = useContext(GlobalContext);
+
+  const query = /* GraphQL */ `
+    query {
+      checkSignedIn {
+        token
+        user {
+          _id
+          username
+          firstname
+          lastname
+          email
+        }
+      }
+    }
+  `;
+
+  useEffect(() => {
+    const check = async () => {
+      const res = await fetchy.post('', {
+        query
+      });
+      const data = await res.json();
+      dispatch(setUser(data.data.checkSignedIn.user));
+      dispatch(setToken(data.data.checkSignedIn.token));
+    };
+    if (!user) {
+      check();
+    }
+  }, [user]);
+
   return (
     <>
       <Switch>
