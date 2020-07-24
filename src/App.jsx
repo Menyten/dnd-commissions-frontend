36603f1489d54d426bf23a4a1282b4e80c451a05
joel.pedersen97@gmail.com <joel.pedersen97@gmail.com>
2020-07-24 18:03:47 +0200

    started work on shop page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Authenticated from './components/common/authenticated';
 import Toast from './components/common/toast';
 import Start from './views/start';
 import Login from './views/login';
+import Shop from './views/shop';
 import CreateAccount from './views/createAccount';
 import CreateShop from './views/createShop';
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Switch>
+        <Route path="/shop" component={Shop} />
         <Route path="/create-shop" component={CreateShop} />
         <Authenticated path="/create-account" component={CreateAccount} />
         <Authenticated path="/login" component={Login} />
---
+++ src/views/shop/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import View from '../../components/common/viewTemplate';
+import Typography from '@material-ui/core/Typography';
+
+const Start = () => {
+  return (
+    <View>
+      <Typography>Current shop page</Typography>
+    </View>
+  );
+};
+
+export default Start;
